b88356ba84948410c2744ecd1b8263345b987827
SH1NDEM <willmirva@gmail.com>
2026-02-01 17:44:35 +0700

    feature(HTTP): add base methods

---
+++ TODO-API/Program.cs
@@ -0,0 +1,45 @@
+using Microsoft.EntityFrameworkCore;
+using TODO_API;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<TaskDb>(opt => opt.UseInMemoryDatabase("TaskItemList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Swagger middleware
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Получить все задачи
+app.MapGet("/taskitems", async (TaskDb db) => 
+    await db.Tasks.ToListAsync());
+
+// Получить все выполненные задачи
+app.MapGet("/taskitems/iscomplite", async (TaskDb db) =>
+    await db.Tasks.Where(t => t.IsClose).ToListAsync());
+
+// Получить все не выполненные задачи
+app.MapGet("/taskitems/isnotcomplite", async (TaskDb db) =>
+    await db.Tasks.Where(t => t.IsClose == false).ToListAsync());
+
+// Получить задачу по id
+app.MapGet("/taskitems/{id}", async (int id, TaskDb db) => 
+    await db.Tasks.FindAsync(id));
+
+// Создать задачу
+app.MapPost("/taskitems", async (TaskItem task, TaskDb db) =>
+{
+    db.Tasks.Add(task);
+    await db.SaveChangesAsync();
+
+    return Results.Created($"/taskitems/{task.Id}", task);
+});
+app.Run();
