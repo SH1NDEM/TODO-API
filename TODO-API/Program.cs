f770ebcfc894b56bf9508c39338ba1ab994afc28
SH1NDEM <willmirva@gmail.com>
2026-02-02 17:51:00 +0700

    feature(add): HTTP methods

--- TODO-API/Program.cs
+++ TODO-API/Program.cs
@@ -42,4 +42,29 @@ app.MapPost("/taskitems", async (TaskItem task, TaskDb db) =>
 
     return Results.Created($"/taskitems/{task.Id}", task);
 });
+
+// Изменение задачи по id
+app.MapPut("/taskitems/{id}", async (TaskItem inputTask, TaskDb db, int id) =>
+{
+    var taskItem = await  db.Tasks.FindAsync(id);
+    if (taskItem is null) return Results.NotFound();
+
+    taskItem.Text = inputTask.Text;
+    taskItem.IsClose = inputTask.IsClose;
+
+    await db.SaveChangesAsync();
+    return Results.Created($"/taskitems/{inputTask.Id}", inputTask);
+});
+
+// Удаление задачи по id
+app.MapDelete("/taskitems/{id}", async (TaskDb db, int id) =>
+{
+    if (await db.Tasks.FindAsync(id) is TaskItem taskItem)
+    {
+        db.Tasks.Remove(taskItem);
+        await db.SaveChangesAsync();
+    }
+    return Results.NoContent();
+});
+
 app.Run();
